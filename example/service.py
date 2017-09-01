0e47c12a805daf162f37f5491b19534d9cd23e16
Kilna <akilna@magento.com>
2017-08-31 22:22:49 -0500

    Make handler code more compact

--- example/service.py
+++ example/service.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-
 import subprocess
-import json
-
 def handler(event, context):
-  font=event.get('font', 'Small')
-  text=event.get('text', '')
-  return subprocess.check_output( ['xart', '-f', font, text ] )
+  return subprocess.check_output( ['xart', '-f', event.get('font', 'Small'), event.get('text', '') ] )
